7c4fb1359f19fcb6e2313654c4f68342445f7a69
Alex Weiss <alex.m.weiss2@gmail.com>
2019-10-01 19:38:48 -0400

    add today's end state for student

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Student, Transaction, StudentDateInOutStatus
 from django.contrib.auth import get_user_model
+from datetime import datetime, timezone, date
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -20,9 +21,16 @@ class StudentDateInOutStatusSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['date', 'in_school', 'resolved']
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    end_states = serializers.SerializerMethodField('get_end_states')
+
+    def get_end_states(self, student):
+        qs = student.end_states.all().filter(date=date.today())
+        serializer = StudentDateInOutStatusSerializer(instance=qs, many=True)
+        return serializer.data
+
     class Meta:
         model = Student
-        fields = ['student_id', 'name', 'grade', 'privilege_granted', 'pathToImage']
+        fields = ['student_id', 'name', 'grade', 'privilege_granted', 'pathToImage', 'end_states']
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     student = StudentSerializer()
