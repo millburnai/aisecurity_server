8dc89f343e98ff4b340594c65b3193cc8ffe13c9
Liam <22pilarskil@millburn.org>
2021-01-29 16:06:21 -0500

    Install redis-server

--- setup.sh
+++ setup.sh
@@ -4,3 +4,18 @@ pip3 install channels
 pip3 install django==2.2
 pip3 install pymysql
 pip3 install websocket-client
+pip3 install channels-redis
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+
+make test
+
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
+
+if [ $1 ] ; then
+	redis-server
+fi
