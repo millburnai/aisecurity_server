5d601ccb5823ff0ad0508b0159f3daaf1d4f4b95
Dhruv Pande <dhruvpande4@gmail.com>
2021-01-30 15:43:45 -0500

    Checks if you already have the server

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
+if [ -f "/usr/local/bin/redis-server" ]
+then
+	echo "You have the server!"
+else
+	wget http://download.redis.io/redis-stable.tar.gz
+	tar xvzf redis-stable.tar.gz
+	cd redis-stable
+	make
+
+	make test
+
+	sudo cp src/redis-server /usr/local/bin/
+	sudo cp src/redis-cli /usr/local/bin/
+fi
+
+
+
 pip3 install django_auditlog
 pip3 install djangorestframework
 pip3 install channels
@@ -6,15 +23,6 @@ pip3 install pymysql
 pip3 install websocket-client
 pip3 install channels-redis
 
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-
-make test
-
-sudo cp src/redis-server /usr/local/bin/
-sudo cp src/redis-cli /usr/local/bin/
 
 if [ $1 ] ; then
 	redis-server
